29cac391ecd0d90dab5ebcbaf244887e8ee9ec3a
binbin <binbin@funnysafe.com>
2016-05-06 10:08:22 +0800

    first commit

---
+++ getui/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+""" create at 5/5/16 """
+
+__author__ = 'binbin'
---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+""" create at 5/5/16 """
+
+__author__ = 'binbin'
+
+from distutils.core import setup
+
+setup(name='getui',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='shaobenbin',
+      author_email='shaobenbin@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['getui', 'getui.google','getui.igetui','getui.payload','getui.protobuf','getui.google.protobuf','getui.google.protobuf.compiler','getui.google.protobuf.internal','getui.google.protobuf.pyext','getui.igetui.template','getui.igetui.utils'],
+     )
